26feb2b034b68ac204991f871975c25dd89ee974
tomsains <thomas.sainsbury@epfl.ch>
2023-02-06 02:01:28 -0800

    socket working - ready for cleanup

---
+++ mouse_task/mouse_manual_water.py
@@ -0,0 +1,115 @@
+'''
+Task to prime (or clean) water line
+
+GK 05/24/19
+'''
+
+from teensyexp.tasks_abc.gui_task import GuiTask
+from tkinter import Tk, Toplevel, Label, Entry, Button, Radiobutton, IntVar, BooleanVar, StringVar
+import time
+import threading
+
+
+class ManualWater(GuiTask):
+
+
+    def __init__(self, teensy):
+        super().__init__(teensy)
+        self.keep_going = True
+        self.n_delivered = 0
+
+
+    def deliver_water(self):
+
+        if self.n_delivered < self.current_pulses:
+            self.teensy.write('water', [self.current_reward])
+            self.n_delivered += 1
+
+            if self.continuous.get():
+                self.window.after(self.wait, self.deliver_water)
+            else:
+                self.n_delivered = 0
+        else:
+            self.n_delivered = 0
+
+
+    def close(self):
+        self.keep_going = False
+
+
+    def change_rate(self, var, idx, mode):
+        try:
+            self.current_rate = float(self.rate.get())
+            self.get_wait()
+        except Exception:
+            pass
+
+
+    def change_reward(self, var, idx, mode):
+        try:
+            self.current_reward = int(self.reward.get())
+            self.get_wait()
+        except Exception:
+            pass
+
+
+    def change_pulse(self, var, idx, mode):
+        try:
+            self.current_pulses = int(self.n_pulses.get())
+        except Exception:
+            pass
+
+
+    def get_wait(self):
+        self.wait = int((1000 / self.current_rate) - self.current_reward)
+
+
+    def create_gui(self, parent):
+
+        window = Toplevel(parent)
+        window.title("Manual Water Delivery")
+        cur_row = 0
+
+        Label(window, text="Pump Time (ms): ").grid(row=cur_row, column=0)
+        self.reward = IntVar(window, value=250)
+        self.current_reward = self.reward.get()
+        self.reward.trace_add("write", self.change_reward)
+        Entry(window, textvariable=self.reward).grid(row=cur_row, column=1)
+        cur_row += 1
+
+        Label(window, text="Continuous Rate (Hz): ").grid(row=cur_row, column=0)
+        self.rate = StringVar(window, value="2")
+        self.current_rate = float(self.rate.get())
+        self.get_wait()
+        self.rate.trace_add("write", self.change_rate)
+        Entry(window, textvariable=self.rate).grid(row=cur_row, column=1)
+        cur_row += 1
+
+        Label(window, text="Number of Pulses: ").grid(row=cur_row, column=0)
+        self.n_pulses = StringVar(window, value="1")
+        self.current_pulses = int(self.n_pulses.get())
+        self.n_pulses.trace_add("write", self.change_pulse)
+        Entry(window, textvariable=self.n_pulses).grid(row=cur_row, column=1)
+        cur_row += 1
+
+        Button(window, text="Deliver One Pulse", command=self.deliver_water).grid(row=cur_row, column=0, rowspan=2)
+        self.continuous = BooleanVar(window, value=False)
+        self.continuous_water = False
+        Radiobutton(window, text="Continuous On", indicatoron=0, variable=self.continuous, value=True, command=self.deliver_water).grid(sticky="nsew", row=cur_row, column=1)
+        Radiobutton(window, text="Continuous Off", indicatoron=0, variable=self.continuous, value=False, command=lambda: setattr(self, 'continuous_water', False)).grid(sticky="nsew", row=cur_row+1, column=1)
+        cur_row += 2
+
+        Button(window, text="Close", command=self.stop).grid(row=cur_row, column=0, columnspan=2)
+
+        self.window = window
+        return window
+
+
+    def loop(self):
+        return self.keep_going, {}
+
+
+    def stop(self):
+        self.continuous_water = False
+        self.keep_going = False
+        super().stop()
--- teensyexp/tasks_abc/dlc_socket.py
+++ teensyexp/tasks_abc/dlc_socket.py
@@ -1,50 +1,29 @@
-import os
+
 import numpy as np
 
 import time
 import threading
 
 from multiprocessing.connection import Client
-from array import array
+
 import numpy as np
 import time
 
-class DLC_client(object):
-
-
+class DLCClient(object):
     def __init__(self, address = ('localhost', 6000)):
         self.address = address
-        self.time_step = []
-        self.x = []
-        self.y = []
+        
         self.reading = True
         self.input_data = []
-        self.output_data = []
+       
         self.start_read_buffer()
-        while self.reading == True:
-            try:
-                data = self.read()
-                if data != None:
-                    print(data)
-                    self.time_step.append(data ["time"])
-                    self.x.append(data ["vals"][0])
-                    self.y.append(data ["vals"][1])
-                time.sleep(0.05)
-            except:
-                break
-            
-
-        
-        data_dict = {"time": self.time_step, "x": self.x, "y": self.y}
-        np.save("/Users/thomassainsbury/Documents/Mathis_lab/socket_test/saved_data/client_tread.npy", data_dict)
-        print("client_thread.py saved")
+       
 
     def read_on_thread(self):
         self.conn = Client(self.address, authkey=b'secret password')
         while self.reading == True: 
             try:
                 this_read = self.conn.recv()
-                
                 self.input_data.append(list((time.time(),this_read)))
             except EOFError:
                 self.reading == False
@@ -59,6 +38,36 @@ class DLC_client(object):
         if self.input_data != []:
             vals = self.input_data[index]
             return({"time": vals[0], "vals": vals [1]})
+    
+    def stop(self):
+        """
+            change the reading class attribute to False (switch flag)
+        """
+        self.reading = False
+
+    def get_input_data(self, format='array'):
+        """
+            Attr:
+                format: ignored
+            returns:
+                input_data list as a numpy array
+        """
+        return np.array(self.input_data)
+
+    def reset(self):
+        """
+            reset to empty list input_data and output_data attributes
+        """
+        self.input_data = []
+        self.output_data = []
+
+    def close(self):
+        """
+            stop serial communication and update reading state attribute to False via stop()
+        """
+        self.stop()
+        self.conn.close()
 
 
-DLC_client()
+
+
