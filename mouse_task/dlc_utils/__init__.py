0b10eb4fe197e0d42d551f870f54f8bdd6056a42
tomsains <thomas.sainsbury@epfl.ch>
2023-02-06 02:05:27 -0800

    clean up

--- mouse_task/dlc_utils/__init__.py
+++ mouse_task/dlc_utils/__init__.py
@@ -1,4 +1,3 @@
 #from .dlcProcessor import MyProcessor
-from .dlcProcessor_2 import MyProcessor2
+from .dlcProcessor_dlconly import MyProcessor2
 from .dlc_processor_socket import MyProcessor_socket
-from .gar_processor import TeensyLaser
