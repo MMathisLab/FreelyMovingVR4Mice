20f7149c54370681a1dce873f5ce61e42c22a83c
MacKenzie Mathis <mackenzie@MacKenzies-MacBook-Air.local>
2023-10-18 17:18:40 +0200

    repo merge

---
+++ AugmentedReality/Assets/scripts/scripts/PhotodiodeChange.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotodiodeChange : MonoBehaviour
+{
+    private Image squareImage;
+    private Color blackColor = Color.black;
+    private Color whiteColor = Color.white;
+    public float sync_state = 0;
+
+    private void Start()
+    {
+        squareImage = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        ChangeColor();
+        if (squareImage.color == Color.black){
+            sync_state = 1f;
+        } else {
+            sync_state = 2f;
+        } 
+    }
+
+    private void ChangeColor()
+    {
+        squareImage.color = squareImage.color == blackColor ? whiteColor : blackColor;
+
+    }
+}
