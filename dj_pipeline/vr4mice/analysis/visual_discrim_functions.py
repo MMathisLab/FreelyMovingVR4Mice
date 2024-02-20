39219e473dcbe4ddda818d899b77dff75a556a3a
tomsains <thomas.sainsbury@epfl.ch>
2024-02-20 16:17:19 +0100

    removed space for celia

--- dj_pipeline/vr4mice/analysis/visual_discrim_functions.py
+++ dj_pipeline/vr4mice/analysis/visual_discrim_functions.py
@@ -144,10 +144,8 @@ def get_all_tolias_mice(mouse_list, path):
 
 
 def get_spatial_normalisation_params(data):
-    data ["norm_head_dir"] = data.groupby("trial", as_index=False)["head_dir"].transform(lambda x: x - x.iloc[0])
-    data ["rewarded"] = data.groupby(["trial"], as_index=False)["reward"].transform("max")["reward"]
-        #data = data [data.rewarded > 0.0].copy()
-    data ["trial_length"] = data.groupby(["trial"], as_index=False)["step_time"].transform(lambda x: x.iloc[-1]-x.iloc[0])["step_time"]
+    data["norm_head_dir"] = data.groupby("trial", as_index=False)["head_dir"].transform(lambda x: x - x.iloc[0])
+    data["trial_length"] = data.groupby(["trial"], as_index=False)["step_time"].transform(lambda x: x.iloc[-1]-x.iloc[0])["step_time"]
     data["bins"] = pd.cut(data["y"], bins = np.linspace(-10,24,50)) 
     data["norm_y"] = data.groupby("trial", as_index=False)["y"].transform(lambda x: x - x.iloc[0])
     data["norm_x"] = data.groupby("trial", as_index=False)["x"].transform(lambda x: x - x.iloc[0])
