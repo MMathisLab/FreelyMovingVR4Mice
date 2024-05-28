11c857e470d32a63d783f9a3523ac3cec3528414
tomsains <thomas.sainsbury@epfl.ch>
2024-05-28 15:09:31 +0200

    dlc processor changes

--- mouse_task/dlc_utils/dlc_processor_socket_pd.py
+++ mouse_task/dlc_utils/dlc_processor_socket_pd.py
@@ -13,7 +13,7 @@ from dlclive import Processor
 from math import sqrt, acos, atan2, copysign, pi, degrees
 
 class MyProcessor_socket(Processor):
-    def __init__(self, com = "/dev/tty.usbmodem146854901", baudrate=9600):
+    def __init__(self, com = "/dev/tty.usbmodem146854901", baudrate=9600, signal_delay = 3, signal_type ="sin"):
         super().__init__()
        # self.queue = queue
         self.teensy = TeensyLatency(com= com, baudrate=baudrate)
@@ -32,6 +32,9 @@ class MyProcessor_socket(Processor):
         self.pose_time =  deque()
         self.curr_step = 0
         self.curr_signal = 0
+        self.start_time = time.time()
+        self.signal_type = signal_type
+        self.
         
     def process(self, pose, **kwargs):
         xy = pose[:, :2]
@@ -49,7 +52,9 @@ class MyProcessor_socket(Processor):
             head_angle = acos(body_axis @ head_axis) * sign
         except ValueError:
             head_angle = 0
-        if self.time_stamp [-1] - self.time_stamp[0] < 3:
+            
+        self.curr_time = time.time()
+        if self.curr_time - self.start_time < 3:
             self.curr_signal = 0
         else:
             self.curr_signal = np.sin(self.curr_step)
@@ -60,18 +65,20 @@ class MyProcessor_socket(Processor):
         heading = degrees(heading)
         vals = *center, heading % (360), head_angle, self.curr_signal
         
+        
         self.center_x.append(vals [0])
         self.center_y.append(vals [1])
         self.heading_direction.append(vals [2])
         self.head_angle.append(vals [3])
-        self.time_stamp.append(time.time())
+        self.time_stamp.append(self.curr_time)
         self.step.append(self.curr_step)
         self.signal.append(self.curr_signal)
+        self.frame_time.append(kwargs ["frame_time"])
+        self.pose_time.append(kwargs ["pose_time"])
         
         self.conn.send([time.time(), vals [0], vals [1], vals [2], vals [3], vals [4]])
      
         return pose
-    
     
     def get_nhz_pulse(self, curr_time, st, freq):
         if (curr_time - st) < self.signal_delay_time:
@@ -95,9 +102,9 @@ class MyProcessor_socket(Processor):
             curr_signal = 0
         else:
             if self.curr_signal == 0:
-            curr_signal = 1
+                curr_signal = 1
             else:
-            curr_signal = 0
+                curr_signal = 0
         return(curr_signal)
         
     
@@ -121,6 +128,8 @@ class MyProcessor_socket(Processor):
     def save_latency_data(self):
         save_dict =  dict()
         save_dict ["start_time"] = np.array(self.st)
+        save_dict ["frame_time"] = np.array(self.frame_time)
+        save_dict ["pose_time"] = np.array(self.pose_time)
         save_dict ["time_stamp"] = np.array(self.time_stamp)
         save_dict ["step"] =  np.array(self.step)
         save_dict ["signal"] = np.array(self.signal)
