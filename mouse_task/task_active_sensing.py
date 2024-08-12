a4e9d826b7d57d8e9db59f28b369f81d6dee9241
tomsains <thomas.sainsbury@epfl.ch>
2024-08-12 14:27:30 +0200

    added new task for spawning multiple occluders, added in documentation and tests

--- mouse_task/task_active_sensing.py
+++ mouse_task/task_active_sensing.py
@@ -53,7 +53,7 @@ class ActiveSensingTask(UnityTask):
     prob_block_coherence: Float, this is the probability that the OOI will appear on the same side as the block. ie if the block was a left block and the prob_block_coherence was 1 then it would appear on the left (default is 0.5). This parameter is only used if the block length is greater that 1.
     mouse_report_delay: Float, mouse report delay default is `0`.
     slit_size: List, this is a list of numbers [min_slit_size, max_slit_size, number_of_slit_sizes] ie. [10,20,5] would give a range of 5 slit sizes with 10 being the minimum and 20 being the max.
-    slit_depth: Float, this parameter controls the depth or thickness of the walls (default = 0.2)
+    slit_depth: Float, this parameter controls the depth or thickness of the walls (default = 0.2). If you want to pass a custom number on multiple slit sizes you can pass this in as a list of numbers ie [12,8,6,5,3] as long as the len of that list if > 3
     target_selection: Integer, this parameter selects what object for the OOI (`0.` = white cube, `1.` = black cube, `2.` = teardrop grey, `3.` = pacman grey, `4.` = teardrop black, `5.` = pacman black, `6.` = teardrop white, `7.` = pacman white,`8.`= zebra teardrop, `9.`= zebra ball, `10.`=white ball, `11.`=light gray zebra teardrop, `12.` = dark gray zebra teardrop )
     distractor_selection: Integer, this parameter selects what object for the distractor (`0.` = white cube, `1.` = black cube, `2.` = teardrop grey, `3.` = pacman grey, `4.` = teardrop black, `5.` = pacman black, `6.` = teardrop white, `7.` = pacman white,`8.`= zebra teardrop, `9.`= zebra ball, `10.`=white ball, `11.`=light gray zebra teardrop, `12.` = dark gray zebra teardrop )
     occlusion_type: Integer, allows the user to select the type of occlusion that they want to use. (`0` = no occlusion, `1` = slit occlusion, `2` = central wall), default is no occlusion.
@@ -160,9 +160,7 @@ class ActiveSensingTask(UnityTask):
         self.reward_size = self.as_list(reward_size)
         self.slit_size = slit_size
         slit_sizes_list = self.as_list(slit_size)
-        self.slit_sizes = np.linspace(
-            slit_sizes_list[0], slit_sizes_list[1], int(slit_sizes_list[2])
-        )
+        self.slit_sizes = self.get_slit_sizes(slit_sizes_list)
 
         self.slit_depth = self.as_list(slit_depth)
         self.slit_depth_param = slit_depth
@@ -425,6 +423,27 @@ class ActiveSensingTask(UnityTask):
                 [0.0, 1.0], p=[1 - self.prob_block_coherence, self.prob_block_coherence]
             )
         print("object on left", self.object_on_left)
+        
+    def get_slit_sizes(self, slit_sizes_list):
+    # Check if the list is empty
+        if len(slit_sizes_list) == 0:
+            raise ValueError("slit_sizes_list cannot be empty.")
+        
+        # Check if the list has fewer than 3 elements
+        if len(slit_sizes_list) < 3:
+            raise ValueError("slit_sizes_list must have at least 3 elements.")
+        
+        # Ensure all elements in slit_sizes_list are numeric
+        if not all(isinstance(x, (int, float)) for x in slit_sizes_list):
+            raise TypeError("All elements in slit_sizes_list must be numeric.")
+        
+        if len(slit_sizes_list) == 3:
+            data = np.linspace(slit_sizes_list[0], slit_sizes_list[1], int(slit_sizes_list[2]))
+        else:
+            data = np.array(slit_sizes_list)
+        
+        return data
+            
 
     def reset_environment(self):
         """
