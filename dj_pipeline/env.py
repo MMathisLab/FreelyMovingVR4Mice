9fc3bc926292f4f2255de12f4990d8bc3f2e46f7
tomsains <thomas.sainsbury@epfl.ch>
2024-08-28 10:18:40 +0200

    edited env - created copy tom env

--- dj_pipeline/env.py
+++ dj_pipeline/env.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["DJ_HOST"] = "localhost"
-os.environ["DJ_USER"] = "user_name"
-os.environ["DJ_PWD"] = "user_name"
+os.environ["DJ_USER"] = "user"
+os.environ["DJ_PWD"] = "password"
 os.environ["DJ_SUPPORT_FILEPATH_MANAGEMENT"] = "TRUE"
 os.environ["DJ_SUPPORT_ADAPTED_TYPES"] = "TRUE"
