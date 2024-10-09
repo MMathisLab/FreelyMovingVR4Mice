067d1330004f1e9a3b9c6a861878082ea932f480
tomsains <thomas.sainsbury@epfl.ch>
2024-10-09 10:24:05 +0200

    swapped indexes of state keys to reflect the game

--- dj_pipeline/vr4mice/actions/helpers_dj.py
+++ dj_pipeline/vr4mice/actions/helpers_dj.py
@@ -99,8 +99,8 @@ def get_state(raw_data=None, key=None, **kwargs):
         "head_dir": 2,
         "mouse_can_report": 3,
         "iti": 4,
-        "mouse_report_correct": 5,  # todo: check idx
-        "obj_left": 6,
+        "obj_left": 5,
+        "mouse_report_correct": 6,  
         "report_left": 7,
         "report_right": 8,
         "velocity": 9,
