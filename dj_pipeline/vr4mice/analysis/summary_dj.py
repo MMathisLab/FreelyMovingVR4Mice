817e60b850642e7e28e4dd1345942b5ff15ec6cb
Célia Benquet <32598028+CeliaBenquet@users.noreply.github.com>
2024-10-15 12:34:09 +0200

    Run black==22.8 and reverted formatter.yml

--- dj_pipeline/vr4mice/analysis/summary_dj.py
+++ dj_pipeline/vr4mice/analysis/summary_dj.py
@@ -10,6 +10,7 @@ from scipy import stats
 from scipy.interpolate import CubicSpline
 
 from vr4mice.schema import base_analysis, vr4mice
+
 # from scipy.signal import savgol_filter, hilbert, find_peaks
 from vr4mice.utils.logger import Logger
 
