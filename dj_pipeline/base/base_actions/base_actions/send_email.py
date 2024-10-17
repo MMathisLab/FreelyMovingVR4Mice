4ff3c856c34808976b0278a4a21617c101c16737
maria <mariaplettre@gmail.com>
2024-10-17 16:55:32 +0200

    Adjust email function messages

--- dj_pipeline/base/base_actions/base_actions/send_email.py
+++ dj_pipeline/base/base_actions/base_actions/send_email.py
@@ -18,19 +18,20 @@ config.read("config.ini")  # Update password path if necessary
 
 
 def email(
+    key,
     email,
     filename,
     message,
     path="/data/processeddata/summaryImgs",
     error=False,
-    pipeline_name="ar_games",
+    pipeline_name="AR pipeline",
 ):
-    
+
     if email == "default":
-        email =  fromaddr
+        email = fromaddr
 
     logger.info(f"Sending email to {email}, file {filename}")
-    
+
     try:
         email_password = config.get("Email", "password")
         fromaddr = config.get("Email", "email")
@@ -40,19 +41,21 @@ def email(
         return
 
     toaddr = [fromaddr, email] if email and (email != fromaddr) else [fromaddr]
-
     msg = MIMEMultipart()
     msg["From"] = fromaddr
     msg["To"] = ", ".join(toaddr)
 
     text = msg.as_string()
 
+    mouse = key["mouse_name"]
+    day = key["day"]
+    attempt = key["attempt"]
     if error:
-        subject = f"Sorry, the pipeline failed for Mouse: {filename.split('_')[1]}, Training performance on {filename.split('_')[2]} and {filename.split('_')[3].split('.')[0]}"
+        subject = f"{pipeline_name} pipeline failed for Mouse: {mouse}, Training performance on day {day} and attempt {attempt}."
         body = message
     else:
-        subject = f"Mouse: {filename.split('_')[1]}. Training Performance on {filename.split('_')[2]} {filename.split('_')[3].split('.')[0]}"
-        body = f"Please find attached the {pipeline_name} summary report for the mouse {filename.split('_')[1]}. The training performance reported here is on {filename.split('_')[2]} and {filename.split('_')[3].split('.')[0]}"
+        subject = f"{pipeline_name}: Mouse: {mouse}. Training Performance on day {day}, attempt {attempt}."
+        body = f"Please find attached the {pipeline_name} summary report for the mouse {mouse}. The training performance reported here is on {day} and attempt {attempt}"
         msg.attach(MIMEText(body, "plain"))
 
         try:
