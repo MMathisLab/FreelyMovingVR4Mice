824b9ec33eeb0ee29d1603da1e08bc236d9d82d1
maria <mariaplettre@gmail.com>
2025-01-10 13:24:20 +0100

    Add new env variables in .env

--- dj_pipeline/env.py
+++ dj_pipeline/env.py
@@ -3,5 +3,7 @@ import os
 os.environ["DJ_HOST"] = "localhost"
 os.environ["DJ_USER"] = "user_name"
 os.environ["DJ_PWD"] = "user_name"
+os.environ["DJ_LAB"] = "lab_name"
+os.environ["GUI"] = "gui_mode"
 os.environ["DJ_SUPPORT_FILEPATH_MANAGEMENT"] = "TRUE"
 os.environ["DJ_SUPPORT_ADAPTED_TYPES"] = "TRUE"
