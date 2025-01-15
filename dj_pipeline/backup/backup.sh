ef9561ff4d4ea5bc28539783a536a012aa770925
maria <mariaplettre@gmail.com>
2025-01-15 14:33:14 +0100

    Add per table backup option.

--- dj_pipeline/backup/backup.sh
+++ dj_pipeline/backup/backup.sh
@@ -4,13 +4,13 @@ source utils.sh
 
 log "Start backup procedure!"
 
-#Get a list of all databases from the source host
 databases=$(mysql $group$s1_prefix $ssl -e "SHOW DATABASES;" |
-	grep -Ev "(Database|information_schema|performance_schema|mysql|sys)")
+	grep -Ev "(Database|information_schema|performance_schema|mysql|sys|base|base_analysis|dlc)")
 
 log "Detected databases to backup: $databases"
 
-# Loop through each database and perform mysqldump on the source host
+databases=(dlc)
+
 #for db in "${databases[@]}"; do
 for db in $databases; do
 
@@ -27,10 +27,41 @@ for db in $databases; do
 	msg="Dumping and importing database: $db"
 
  cmd="mysqldump $group$s1_prefix $ssl --ignore-table='vr4mice'.'~log'--ignore-table='dlc'.'~log' \
-             	--single-transaction --max-allowed-packet=512M --quick --skip-add-drop-table --insert-ignore "$db" \
+             	--single-transaction --quick --max_allowed_packet=512M --skip-add-drop-table --insert-ignore "$db" \
                 | sed 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g' \
  		            | sed 's/INSERT INTO/INSERT IGNORE INTO/g' \
 		            | mysql  $group$aws_prefix $db"
     
- execute_command "$cmd" "$msg"
+ #execute_command "$cmd" "$msg"
+
+ # Step 1: Dump the database to an intermediate file and modify it
+ cmd1="mysqldump $group$s1_prefix $ssl --ignore-table='vr4mice'.'~log' --ignore-table='dlc'.'~log' \
+		--single-transaction --quick --max_allowed_packet=512M --skip-add-drop-table --insert-ignore \"$db\" \
+		> $db.sql && \
+		sed -i 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g' $db.sql && \
+		sed -i 's/INSERT INTO/INSERT IGNORE INTO/g' $db.sql"
+
+ #execute_command "$cmd1" "$msg"
+
+ # Step 2: Import the modified SQL dump into MySQL and clean up the file
+ cmd2="mysql $group$aws_prefix --max_allowed_packet=1G \"$db\" < $db.sql "
+ 
+ #execute_command "$cmd2" "$msg"
+
+tables=$(mysql $group$s1_prefix $ssl -e "SHOW TABLES IN $db;" -s --skip-column-names)
+
+	# Loop through each table
+	for table in $tables; do
+	    
+	    msg="Dumping and importing database: $db, table $table"
+	    
+	    cmd3="mysqldump $group$s1_prefix $ssl --ignore-table='$db'.'~log' --max_allowed_packet=1G $db $table \
+			| sed 's/CREATE TABLE/CREATE TABLE IF NOT EXISTS/g' \
+				    | sed 's/INSERT INTO/INSERT IGNORE INTO/g' \
+				    | mysql $group$aws_prefix $db --max_allowed_packet=1G"
+	    
+	    execute_command "$cmd3" "$msg"
+	    
+	    echo "All chunks processed successfully."
+	done
 done
