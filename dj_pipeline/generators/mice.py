6dbf4f3d1d461c5f42285d3959ff360dd827d1b1
mary <43879378+maryapp@users.noreply.github.com>
2025-02-10 13:28:38 +0100

    DJ fixes (#180)
    * rm class definition duplication
    
    * style: f formatting
    
    * add per lab label
    
    * minor changes
    
    * update gitignore
    
    * fix likelihood typo
    
    * Add new env variables in .env
    
    * Put gui mode via os env
    
    * Add NULL default values.
    
    * Add more default null values.
    
    * Add more default null values for step.
    
    * rename GUI os env.
    
    * Add default null value for signal.
    
    * fix api typo
    
    * vr4mice/schema/dlc.py
    
    * fix 'support for generic buffers has not been implemented' bug
    
    * Add move to api.
    
    * black
    
    * last erd update
    
    * add missing populate
    
    * Add per table backup option.
    
    * Add flags to avoid it to remplace data.
    
    * Add else log case.
    
    * add dj schema as import
    
    * check default gui init
    
    * Change gui alert msg
    
    * Change gui alert msg synax
    
    * Add bool convertion
    
    * Add imagimg dource in os var
    
    * Add new camera type
    
    * Backup default config
    
    * black
    
    * add try catch
    
    * fix dataset
    
    * add basic xlsx mice info parser
    
    * print error too
    
    * black parser
    
    * Add gui manual deploy test template and fix move files old bug
    
    * add mouse script
    
    * support of npy populate only
    
    * return err
    
    * external mice
    ---------
    
    Signed-off-by: mary <43879378+maryapp@users.noreply.github.com>

---
+++ dj_pipeline/generators/mice.py
@@ -0,0 +1,74 @@
+import datetime
+from base_schemas.schemas import mice
+
+
+def insert_mice():
+    mouse_entries = [
+        {
+            "mouse_name": "31436",
+            "mouse_id": 31436,
+            "dob": datetime.date(2024, 3, 5),
+            "sex": "M",
+            "strain": "WT",
+        },
+        {
+            "mouse_name": "31437",
+            "mouse_id": 31437,
+            "dob": datetime.date(2024, 3, 5),
+            "sex": "M",
+            "strain": "WT",
+        },
+        {
+            "mouse_name": "31438",
+            "mouse_id": 31438,
+            "dob": datetime.date(2024, 3, 5),
+            "sex": "M",
+            "strain": "WT",
+        },
+        {
+            "mouse_name": "31537",
+            "mouse_id": 31537,
+            "dob": datetime.date(2024, 5, 21),
+            "sex": "F",
+            "strain": "WT",
+        },
+        {
+            "mouse_name": "31538",
+            "mouse_id": 31538,
+            "dob": datetime.date(2024, 5, 21),
+            "sex": "F",
+            "strain": "WT",
+        },
+        {
+            "mouse_name": "31539",
+            "mouse_id": 31539,
+            "dob": datetime.date(2024, 5, 21),
+            "sex": "F",
+            "strain": "WT",
+        },
+        {
+            "mouse_name": "31475",
+            "mouse_id": 31475,
+            "dob": datetime.date(2024, 7, 1),
+            "sex": "M",
+            "strain": "WT",
+        },
+        {
+            "mouse_name": "31476",
+            "mouse_id": 31476,
+            "dob": datetime.date(2024, 7, 1),
+            "sex": "M",
+            "strain": "WT",
+        },
+    ]
+
+    for mouse in mouse_entries:
+        try:
+            mice.Mouse().insert1(mouse)
+            print(f"Inserted mouse {mouse['mouse_name']} successfully.")
+        except Exception as e:
+            print(f"Error inserting mouse {mouse['mouse_name']}: {e}")
+
+
+if __name__ == "__main__":
+    insert_mice()
