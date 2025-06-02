43c4de065cc8978f30bea0e1dab79148447d2217
maria <mariaplettre@gmail.com>
2025-06-02 18:27:51 +0200

    clean tables

--- dj_pipeline/vr4mice/schema/interpolated_trajectories.py
+++ dj_pipeline/vr4mice/schema/interpolated_trajectories.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 from vr4mice.analysis.analysis import get_jshaped_trials
-from vr4mice.schema import vr4mice, base_analysis
+from vr4mice.schema import vr4mice, base_analysis, dlc
 from vr4mice.utils.logger import Logger
 from vr4mice.utils.schema_config import get_schema
 
@@ -22,31 +22,31 @@ logger = Logger.get_logger()
 class InterpolatedTrials(dj.Computed):
     definition = """
     -> base_analysis.DataFrame
-    mouse_name: longblob # mouse name
-    aperture: longblob # occlusion aperture size
-    trial: longblob # trial number
-    trial_length: longblob # the trial progression points
-    trial_duration: longblob # trial duration
-    trial_tortuosity: longblob # trial tortuosity
+    ---
+    aperture: longblob          # occlusion aperture size
+    trial: longblob             # trial number
+    trial_length: longblob      # the trial progression points
+    trial_duration: longblob    # trial duration
+    trial_tortuosity: longblob  # trial tortuosity
     trial_left_choice: longblob # left or right choice for each trial
-    trial_rewarded: longblob # whether the trial was rewarded
-    x: longblob # x-coordinate of each trial progression point
-    y: longblob # y-coordinate of each trial progression point
-    x_flipped: longblob # x-coordinate of each trial progression point flipped
-    heading_dir: longblob # heading direction of the mouse
-    head_angle: longblob # head angle of the mouse
-    velocity: longblob # velocity of each trial progression point
-    velocity_x: longblob # x-velocity of each trial progression point
-    velocity_y: longblob # y-velocity of each trial progression point
-    flip_one_side: longblob # vector to flip trajectories across the mid line to align choices
+    trial_rewarded: longblob    # whether the trial was rewarded
+    x: longblob                 # x-coordinate of each trial progression point
+    y: longblob                 # y-coordinate of each trial progression point
+    x_flipped: longblob         # x-coordinate of each trial progression point flipped
+    heading_dir: longblob       # heading direction of the mouse
+    head_angle: longblob        # head angle of the mouse
+    velocity: longblob          # velocity of each trial progression point
+    velocity_x: longblob        # x-velocity of each trial progression point
+    velocity_y: longblob        # y-velocity of each trial progression point
+    flip_one_side: longblob     # vector to flip trajectories across the mid line to align choices
     distance_to_choice: longblob # distance to the choice point
-    optimal_p: longblob # optimal p parameter from LP curve fitting
-    local_tortuosity: longblob # local tortuosity of the mouse trajectory
-    head_angle_sin: longblob # sin of the head angle
-    head_angle_cos: longblob # cos of the head angle
-    heading_dir_sin: longblob # sin of the heading direction
-    heading_dir_cos: longblob # cos of the heading direction
-    velocity_x_flipped: longblob # x-velocity of each trial progression point flipped
+    optimal_p: longblob         # optimal p parameter from LP curve fitting
+    local_tortuosity: longblob  # local tortuosity of the mouse trajectory
+    head_angle_sin: longblob    # sin of the head angle
+    head_angle_cos: longblob    # cos of the head angle
+    heading_dir_sin: longblob   # sin of the heading direction
+    heading_dir_cos: longblob   # cos of the heading direction
+    velocity_x_fliped: longblob # x-velocity of each trial progression point flipped
     """
 
     def make(self, key):
@@ -63,12 +63,11 @@ class InterpolatedTrials(dj.Computed):
                 df = (base_analysis.DataFrame()).get_data(key=key)
                 df["trial_rewarded"] = (base_analysis.DataFrame()).get_rewarded(key=key)
                 box_df = (base_analysis.BoxDataFrame()).get_data(key=key)
-                offline_kinematics_df = (vr4mice.dlc.OfflineKinematics()).get_data(
-                    columns=["heading_direction", "head_angle"], key=key
+                offline_kinematics_df = (dlc.OfflineKinematics()).get_data(
+                    columns=["heading_dir", "head_angle"], key=key
                 )
                 df = pd.concat([df, offline_kinematics_df], axis=1)
                 df = df[df.iti == 0.0]
-                df["mouse_name"] = key["dataset"].split("_")[0]
                 interpolated_df = interpolate_j_shaped(df, box_df=box_df)
                 interpolated_df = interpolated_df.drop(
                     columns=["time", "dataset", "trial_step"]
@@ -84,19 +83,20 @@ class InterpolatedTrials(dj.Computed):
             err = f"Can't populate {self.__class__.__name__}, key: {key}. Error: {err}."
             logger.warning(err)
 
-
+@schema
 class MeanXYTrajectory(dj.Computed):
     definition = """
     -> InterpolatedTrials
-    aperture: longblob # occlusion aperture size for each trial progression point
+    ---
+    aperture: longblob          # occlusion aperture size for each trial progression point
     trial_left_choice: longblob # left or right choice for each trial
-    trial_length: longblob # the trial progression points
-    x: longblob # mean x-coordinate of each trial progression point
-    y: longblob # mean y-coordinate of each trial progression point
-    x_sem: longblob # standard error of the mean x-coordinate of each trial progression point
-    y_sem: longblob # standard error of the mean y-coordinate of each trial progression point
-    x_std: longblob # standard deviation of the x-coordinate of each trial progression point
-    y_std: longblob # standard deviation of the y-coordinate of each trial progression point
+    trial_length: longblob      # the trial progression points
+    x: longblob                 # mean x-coordinate of each trial progression point
+    y: longblob                 # mean y-coordinate of each trial progression point
+    sem_x: longblob             # standard error of the mean x-coordinate of each trial progression point
+    sem_y: longblob             # standard error of the mean y-coordinate of each trial progression point
+    std_x: longblob             # standard deviation of the x-coordinate of each trial progression point
+    std_y: longblob             # standard deviation of the y-coordinate of each trial progression point
     """
 
     def make(self, key):
@@ -111,31 +111,28 @@ class MeanXYTrajectory(dj.Computed):
         try:
             if len(InterpolatedTrials & key) > 0:
                 df = pd.DataFrame(
-                    (InterpolatedTrials()).get_data(
-                        columns=[
+                    (InterpolatedTrials() & key).fetch(
                             "dataset",
                             "aperture",
                             "trial",
                             "trial_left_choice",
                             "trial_length",
                             "x",
-                            "y",
-                        ],
-                        key=key,
-                    )
+                            "y", as_dict=True
+                    )[0]
                 )
 
                 mean_df = mean_xy_trajectory(
                     df,
-                    index_col=[
+                    index_columns=[
                         "dataset",
                         "aperture",
                         "trial_left_choice",
                         "trial_length",
                     ],
-                )
-                mean_df = mean_df.drop(columns=["dataset"])
-                self.insert1({**key, **mean_df.to_dict()})
+                ) # returns pd.DataFrame
+                mean_df = (mean_df.drop(columns=["dataset"])).to_dict()
+                self.insert1({**key, **mean_df})
 
         except Exception as err:
             dataset = key["dataset"]
@@ -146,12 +143,15 @@ class MeanXYTrajectory(dj.Computed):
             logger.warning(err)
 
 
+@schema
 class YBinnedXYTrajectory(dj.Computed):
     definition = """
     -> InterpolatedTrials
+    ---
     aperture: longblob # occlusion aperture size 
     x_flipped: longblob # x-coordinate for each y bin center
-    bin_centers: longblob # y bin centers 
+    bin_centers: longblob # y bin centers
+    y: longblob # y
     """
 
     def make(self, key):
@@ -167,22 +167,22 @@ class YBinnedXYTrajectory(dj.Computed):
         try:
             if len(InterpolatedTrials & key) > 0:
                 df = pd.DataFrame(
-                    (InterpolatedTrials()).get_data(
-                        columns=["aperture", "trial", "trial_length", "x_flipped", "y"],
-                        key=key,
-                    )
+                    (InterpolatedTrials() & key).fetch(
+                        "aperture", "trial", "trial_length", "x_flipped", "y",
+                        as_dict=True
+                    )[0]
                 )
                 mean_df = mean_xy_trajectory(
                     df,
-                    index_col=["aperture", "trial_length"],
+                    index_columns=["aperture", "trial_length"],
                     values=["x_flipped", "y"],
                 )
                 binned_df = create_bins(mean_df)
                 binned_df = binned_df[["aperture", "bin_centers", "x_flipped", "y"]]
-                binned_df = binned_df.groupby(
+                binned_df = (binned_df.groupby(
                     ["aperture", "bin_centers"], as_index=False
-                ).mean(numeric_only=True)
-                self.insert1({**key, **binned_df.to_dict()})
+                ).mean(numeric_only=True)).to_dict()
+                self.insert1({**key, **binned_df})
 
         except Exception as err:
             dataset = key["dataset"]
@@ -193,15 +193,17 @@ class YBinnedXYTrajectory(dj.Computed):
             logger.warning(err)
 
 
+@schema
 class MeanVelocities(dj.Computed):
     definition = """
     -> InterpolatedTrials
+    ---
     aperture: longblob # occlusion aperture size 
     trial_length: longblob # trial length
     velocity: longblob # mean velocity of each trial progression point
     velocity_x: longblob # mean x-velocity of each trial progression point
     velocity_y: longblob # mean y-velocity of each trial progression point
-    velocity_x_flipped: longblob # mean x-velocity of each trial progression point flipped
+    velocity_x_fliped: longblob # mean x-velocity of each trial progression point flipped
     """
 
     def make(self, key):
@@ -213,17 +215,15 @@ class MeanVelocities(dj.Computed):
 
         try:
             if len(InterpolatedTrials & key) > 0:
-                df = pd.DataFrame(InterpolatedTrials()).get_data(
-                    columns=[
+                df = pd.DataFrame((InterpolatedTrials() & key).fetch(
                         "aperture",
-                        "trial",
                         "trial_length",
                         "velocity",
                         "velocity_x",
                         "velocity_y",
-                        "velocity_x_flipped",
-                    ],
-                    key=key,
+                        "velocity_x_fliped",
+                    as_dict=True
+                )[0]
                 )
                 mean_df = df.groupby(["aperture", "trial_length"], as_index=False).mean(
                     numeric_only=True
