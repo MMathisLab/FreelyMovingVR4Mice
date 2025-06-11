1954ff2073aaa4bf4652616ced65bc7c72b39d5d
tomsains <thomas.sainsbury@epfl.ch>
2025-06-11 16:53:29 +0200

    updated notebooks

--- dj_pipeline/vr4mice/analysis/utils.py
+++ dj_pipeline/vr4mice/analysis/utils.py
@@ -383,7 +383,7 @@ def multi_occluder_inclusion_criteria(
     ]
     pivoted_reward = pivoted_reward[abs(pivoted_reward.reward_drop) < threshold_drop]
 
-    logger.info(
+    print(
         "Excluded datasets: ",
         data[data.dataset.isin(pivoted_reward.index) == 0].dataset.unique(),
     )
