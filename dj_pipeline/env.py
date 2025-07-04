c9251616f3eeafc1ce1670e6cb0559c06205b844
Célia Benquet <celia.benquet@gmail.com>
2025-07-04 15:02:44 +0200

    Update notebooks with tortuosity and j-shaped ratio

--- dj_pipeline/env.py
+++ dj_pipeline/env.py
@@ -1,14 +1,11 @@
 import os
 
-os.environ["DJ_HOST"] = "128.178.51.167:3309"
-os.environ["DJ_USER"] = "celia"
-os.environ["DJ_PWD"] = "celia"
+os.environ["DJ_HOST"] = "localhost"
+os.environ["DJ_USER"] = "user_name"
+os.environ["DJ_PWD"] = "user_name"
+os.environ["DJ_LAB"] = "lab_name"
+os.environ["GUI"] = "gui_mode"
 os.environ["EMAIL"] = "True"
 os.environ["IMG_SRC"] = "Imagingsource"
 os.environ["DJ_SUPPORT_FILEPATH_MANAGEMENT"] = "TRUE"
 os.environ["DJ_SUPPORT_ADAPTED_TYPES"] = "TRUE"
-
-os.environ["EMAIL"] = "True"
-os.environ["DJ_LAB"] = "mathis-lab"
-os.environ["GUI"] = "True"
-os.environ["IMG_SRC"] = "Imagingsource"
