68f3abe558c6ee7f0f4b77c14cfc030e5ea283f7
Célia Benquet <celia.benquet@gmail.com>
2025-07-24 15:49:53 +0200

    Logger and suppress error in script

--- dj_pipeline/script_populate_celia.py
+++ dj_pipeline/script_populate_celia.py
@@ -1,5 +1,11 @@
 from base_actions.connect import connect
 connect()
 
+from vr4mice.utils.logger import Logger
+logger = Logger.get_logger()
+
+
 from vr4mice.schema.vr4mice import SignalsPhotodiode
-SignalsPhotodiode().populate({"dataset": "Jacana_2024-07-26_1"})
+
+SignalsPhotodiode().populate({"dataset": "Jacana_2024-07-26_1"},
+                             suppress_errors = True)
