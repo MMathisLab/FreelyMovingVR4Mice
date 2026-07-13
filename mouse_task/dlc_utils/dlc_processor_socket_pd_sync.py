8db9d0b756e1bd6e2d9262e42485171b8b86a09c
Célia Benquet <celia.benquet@gmail.com>
2026-07-13 19:30:09 +0200

    Set dlc threshold back to 0.6

--- mouse_task/dlc_utils/dlc_processor_socket_pd_sync.py
+++ mouse_task/dlc_utils/dlc_processor_socket_pd_sync.py
@@ -61,7 +61,7 @@ class dlc_inference_w_pd_sync(dlc_inference_w_pd):
     # sockets / serial / side-effect-heavy resources are created inside DLCLiveWorker.
     PROCESSOR_BUILD_IN_WORKER = True
 
-    HEAD_CONF_THRESHOLD = 0.01
+    HEAD_CONF_THRESHOLD = 0.6 # NOTE: this might need to be adjusted based on the model
 
     PROCESSOR_PARAMS = {
         "com": {
